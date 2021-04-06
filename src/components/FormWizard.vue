02a5ffd797b91830b4320000ebcdef675df12907
Ebenezer Ndukwe <ebnettech@gmail.com>
2021-04-06 20:57:20 +0100

    all states in nigeria added

--- src/components/FormWizard.vue
+++ src/components/FormWizard.vue
@@ -129,13 +129,52 @@
               </div>
               <div class="col-8">
                 <div class="form-group">
-                  <input
+                  <!-- <input
                     v-model="formData.stateofoperation"
                     type="text"
                     id="stateofoperation"
                     class="name-input"
                     disabled
-                  />
+                  /> -->
+                  <select name="" class="name-input">
+                    <option value="Location">Location</option>
+                    <option value="lagos">Lagos</option>
+                    <option value="abia">Abia</option>
+                    <option value="adamawa">Adamawa</option>
+                    <option value="akwaIbom">Akwa Ibom</option>
+                    <option value="anambra">Anambra</option>
+                    <option value="bauchi">Bauchi</option>
+                    <option value="bayelsa">Bayelsa</option>
+                    <option value="benue">Benue</option>
+                    <option value="borno">Borno</option>
+                    <option value="croseRiver">Cross River</option>
+                    <option value="dedlta">Delta</option>
+                    <option value="ebonyi">Ebonyi</option>
+                    <option value="edo">Edo</option>
+                    <option value="ekiti">Ekiti</option>
+                    <option value="enugu">Enugu</option>
+                    <option value="gombe">Gombe</option>
+                    <option value="imo">Imo</option>
+                    <option value="jigawa">Jigawa</option>
+                    <option value="kaduna">Kaduna</option>
+                    <option value="kano">Kano</option>
+                    <option value="katsina">Kastina</option>
+                    <option value="kebbi">Kebbi</option>
+                    <option value="kogi">Kogi</option>
+                    <option value="kwara">Kwara</option>
+                    <option value="nasarawa">Nasarawa</option>
+                    <option value="niger">Niger</option>
+                    <option value="ogun">Ogun</option>
+                    <option value="ondo">Ondo</option>
+                    <option value="osun">Osun</option>
+                    <option value="oyo">Oyo</option>
+                    <option value="plateau">Plateau</option>
+                    <option value="rivers">Rivers</option>
+                    <option value="sokoto">Sokoto</option>
+                    <option value="taraba">Taraba</option>
+                    <option value="yobe">Yobe</option>
+                    <option value="zamfara">Zamfara</option>
+                  </select>
                 </div>
               </div>
             </div>
